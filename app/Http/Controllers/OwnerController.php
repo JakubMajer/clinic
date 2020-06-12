d53f196afcefd9aac4ebc445cd44508ce7349c7a
Jakub Majer <jakub.majer11@gmail.com>
2020-06-12 13:57:46 +0200

    create pet and owner

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -12,15 +12,18 @@ class OwnerController extends Controller
     {
         return view('owners.index');
     }
+    public function show($owner_id){
 
-    public function show()
-    {
-        # code...
+        $owner = Owner::findOrFail($owner_id);
+
+        $books = Book::where('publisher_id', $publisher_id)->get();
+
+        return view('publishers.show', compact('publisher', 'books'));
     }
 
     public function create()
     {
-        # code...
+        return view('owners.create');
     }
 
     public function store()
